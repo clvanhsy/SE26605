20104d888ee54487d16e75bd345632f394b89532
Cynthia Vanhsy <clvanhsy@email.neit.edu>
2017-10-02 19:54:11 -0400

    demo

---
+++ demo.php
@@ -0,0 +1,12 @@
+<h1> Hi </h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cynthia
+ * Date: 10/2/2017
+ * Time: 7:33 PM
+ */
+$first = "Cynthia";
+$last = "Vanhsy";
+echo "$first $last <br />";
+echo $first . " " . $last;
