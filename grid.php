52d6e57da38fcaa707db049a74f33f015ceeb6a8
Cynthia Vanhsy <clvanhsy@email.neit.edu>
2017-10-04 14:27:10 -0400

    grid

---
+++ grid.php
@@ -0,0 +1,40 @@
+<?php
+function randColor() {
+    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+}
+
+function random_color() {
+    return randColor() . randColor() . randColor();
+}
+
+echo random_color();
+
+
+?>
+<?php
+$table = "<table>"; //empty table var
+
+for($rows =0; $rows <= 10; $rows++){
+    $value = random_color();
+$table .= "<tr><td style='background-color:#".$value.";'>$value<br /><span style ='color:#ffffff;'>$value</span></td>";
+
+    for($col = 0; $col <= 9; $col++)
+    {
+        $value = random_color();
+        $table .= "<td style='background-color:".$value.";'>$value<br /><span style ='color:#ffffff;'>$value</span></td>";
+    }
+    $table .="</tr>";
+}
+$table .="</table>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Rand Grid</title>
+</head>
+<body>
+<?php echo $table ?>
+</body>
+</html>
