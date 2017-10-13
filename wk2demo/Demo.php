ecbaec21acbeb2ccc4f5de09fa2a075d007cad8b
Cynthia Vanhsy <clvanhsy@email.neit.edu>
2017-10-12 23:42:37 -0400

    Demo

--- wk2demo/Demo.php
+++ wk2demo/Demo.php
@@ -8,11 +8,11 @@
         $db = new PDO($dsn, $username, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // If There is an issue throw it down to the catch
         $submit = $_GET['submit'] ?? ""; // Null  coalesce operator
-        if('submit' == "Do it") {
+        if($submit == "Do it") {
             $name = $_GET['name'] ?? ""; //_GET  is Global Var
             $gender = $_GET['gender'] ?? "";
             $fixed = $_GET['fixed'] ?? "N";
-            $sql = $db->prepare("INSERT INTO  dogs VALUES (null, :name, :gender, :fixed )");
+            $sql = $db->prepare("INSERT INTO dogs VALUES (null, :name, :gender, :fixed )");
             $sql->bindParam(':name', $name);
             $sql->bindParam(':gender', $gender);
             $sql->bindParam('fixed', $fixed);
