267e646dd3f1968a78a5706ca211fc69c4d04b34
Cynthia Vanhsy <clvanhsy@email.neit.edu>
2017-10-16 08:20:50 -0400

    Basic template

---
+++ wk3demo/assets/footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cynthia
+ * Date: 10/16/2017
+ * Time: 8:15 AM
+ */
+?>
+<footer>
+Copyrights &copy; <?php echo Date('Y'); ?> Cynthia Vanhsy. All rights reserved
+</footer>
---
+++ wk3demo/assets/header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cynthia
+ * Date: 10/16/2017
+ * Time: 8:15 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- css links goes here -->
+    <!-- JavaScript goes here -->
+</head>
+<body>
+<header>
+    <nav>
+
+    </nav>
+</header>
+<section>
+
+</section>
+</body>
+</html>
